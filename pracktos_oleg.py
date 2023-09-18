78a984395a5f8c3445219ab818b69f4ca0dff768
Олег Шевцов <oleg@MacBook-Air-Oleg.local>
2023-09-18 11:00:48 +0300

    kalkulyator

--- pracktos_oleg.py
+++ pracktos_oleg.py
@@ -1,5 +1,5 @@
 import math
-
+div = "---------------"
 while True:
     print("Выберите операцию:")
     print("1. Сложение")
@@ -13,63 +13,96 @@ while True:
     print("9. Косинус")
     print("10. Тангенс")
     print("0. Выход")
-    choice = int(input("Введите номер операции: "))
+    choice = input("Введите номер операции: ")
+
+    if not choice.isdigit():
+        print(div)
+        print("Ошибка. Введите номер операции (целое число).")
+        print(div)
+        continue
+
+    choice = int(choice)
     if choice == 0:
         break
     elif choice == 10:
         a = float(input("Введите число: "))
         if a % 180 == 90:
+            print(div)
             print("Ошибка(Такого значения нет в таблице)")
+            print(div)
         else:
             print(math.tan(a))
     elif choice == 9:
         a = float(input("Введите число: "))
+        print(div)
         print(math.cos(a))
+        print(div)
     elif choice == 8:
         a = float(input("Введите число: "))
+        print(div)
         print(math.sin(a))
+        print(div)
 
     elif choice == 7:
         a = float(input("Введите число: "))
         if a >= 0:
+            print(div)
             print(math.factorial(a))
+            print(div)
         else:
+            print(div)
             print("Ошибка")
+            print(div)
 
     elif choice == 6:
         a = float(input("Введите число: "))
         if a >= 0:
+            print(div)
             print(math.sqrt(a))
+            print(div)
         else:
+            print(div)
             print("Ошибка")
+            print(div)
 
     elif choice == 5:
         a = float(input("Введите число: "))
         b = float(input("Введите степень: "))
+        print(div)
         print(math.pow(a, b))
+        print(div)
 
     elif choice == 4:
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
         if b == 0:
+            print(div)
             print("На нуль делить нельзя")
+            print(div)
         else:
+            print(div)
             print(a/b)
+            print(div)
 
     elif choice == 3:
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
+        print(div)
         print(a*b)
+        print(div)
 
     elif choice == 2:
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
+        print(div)
         print(a-b)
+        print(div)
 
     elif choice == 1:
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
+        print(div)
         print(a+b)
+        print(div)
     else:
-
         break
